6dcdb77297d5b45318c261e271136afae018513d
The_Lost_Owl <60459107+vungoctuyen1812@users.noreply.github.com>
2020-06-23 12:58:55 +0900

    Update newest code

--- BackEndPhp/homework-1-bien-moi-truong/index.php
+++ BackEndPhp/homework-1-bien-moi-truong/index.php
@@ -1,17 +1,61 @@
 <?php 
-session_start();
+	session_start();
+	if ( isset ($_POST['submitLogin']) ){
+		if ( $_POST['username'] == 'thelostowl' && $_POST['password'] == '123456'){
+			$_SESSION['username'] = $_POST['username'];
+			echo ('<script>alert("Login Success")</script>');
+			header('location: adminPage.php');
+		}else {
+			echo ('<script>alert("Login Failed")</script>');
+		}
+			
+		
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="style.css">
+	
+	<title>Login Page</title>
 
-if ( isset($name) ){
-	echo 'The varible is none  defined';
-}else echo 'OK';
- ?>
-}
-<form method="post" action="index.php">
-	<div class="user-name">
-		User Name: <input type="text" name="username" placeholder="User Name">
+</head>
+<body>
+	<div class="content-wrapper">
+		<h3>Steam Login</h3>
+		<div class="form-login">
+			<form method="post" action="index.php">
+				<div class="item">
+					<div class="input-login">
+						<div class="icon">
+							<span class="iconify" data-icon="carbon:user-avatar-filled" data-inline="false"></span>
+						</div>
+						<input type="text" name="username" >
+					</div>
+				</div>
+				<div class="item">
+					<div class="input-login">
+						<div class="icon">
+							<span class="iconify" data-icon="ri:lock-password-fill" data-inline="false"></span>
+						</div>
+						<input type="password" name="password" >
+					</div>
+				</div>
+				<div class="item">
+					<div class="btn-login">
+						<button type="submid" name="submitLogin" value="abc" class="lgn-btn">Login</button>
+						<button>Cancel</button>
+					</div>
+					
+				</div>
+				
+			</form>
+		</div>
 	</div>
-	<div class="password">
-		Password: <input type="password" name="password" placeholder="Password">
-	</div>
-	<button type="submid" name="submit-login" value="abc"></button>
-</form>
+
+<script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>
+</body>
+</html>
+
+
